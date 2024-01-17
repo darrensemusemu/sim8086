e47d312bebb327247ef05e5f7c6c3fe2c4d28d2e
Darren Semusemu <darrensemusemu@gmail.com>
2024-01-17 19:25:58 +0000

    update test.sh as single test cmd

--- test.sh
+++ test.sh
@@ -4,12 +4,21 @@
 
 set -e
 
-if [[ -z "$1" || -z "$2" ]]; then
+if [[ -z "$1" ]]; then
     echo "USAGE: $0 <file1>.asm <file2>"
     exit 1
 fi
 
-file_name="$1_output"
-nasm $1 -o "$file_name"
-cmp $file_name $2
+
+decode_output_file="/tmp/sim8086.txt"
+nasm_output_file="/tmp/sim8086.asm"
+
+zig build run -- decode $1 > "$decode_output_file"
+nasm "$decode_output_file" -o "$nasm_output_file"
+
+if ! cmp $nasm_output_file $1 ; then
+    echo "FAILED: out files do not match"
+else
+    echo "SUCCESS: files match"
+fi
 
